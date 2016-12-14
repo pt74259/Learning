5f4c8d8352dcc14f40a74aa01372ed2ab79816ed
P T <patrick.tomei89@gmail.com>
2016-12-14 12:05:59 +0900

    Add files via upload

---
+++ ex15-1.py
@@ -0,0 +1,12 @@
+txt = open(raw_input("> "))
+filename = txt
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input ("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
